6c12f22e6a75ff89520aedadfcd3eae72a69e709
maxtyutmanov <tyutmanovmd@gmail.com>
2016-01-28 23:34:27 +0300

    added skeleton of DocService project

---
+++ DocService/DocService.Core/Engine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocService.Core
+{
+    public class Engine
+    {
+        
+
+        private readonly IFsWatcher _watcher;
+        private readonly IStorageConnector _storageConnector;
+
+        public Engine(IFsWatcher watcher, IStorageConnector storageConnector)
+        {
+            
+            _watcher = watcher;
+            _storageConnector = storageConnector;
+        }
+
+        public void Start()
+        {
+            
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DocService/DocService.Core/NewFileAddedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocService.Core
+{
+    public class NewFileAddedEventArgs : EventArgs
+    {
+        public DocInfo AddedDoc { get; private set; }
+
+        public NewFileAddedEventArgs(DocInfo addedDoc)
+        {
+            Contract.Requires(addedDoc != null);
+
+            this.AddedDoc = addedDoc;
+        }
+    }
+}
